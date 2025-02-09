41cbf484b765c3adbe485d9455bc577b1746cfc4
veortiz1 <veortiz1@asu.edu>
2025-02-08 19:37:32 -0700

    add modules

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+    console.log("Hello");
+    const nbaTeams = [
+        "Atlanta Hawks",
+        "Boston Celtics",
+        "Brooklyn Nets",
+        "Charlotte Hornets",
+        "Chicago Bulls",
+        "Cleveland Cavaliers",
+        "Dallas Mavericks",
+        "Denver Nuggets",
+        "Detroit Pistons",
+        "Golden State Warriors",
+        "Houston Rockets",
+        "Indiana Pacers",
+        "Los Angeles Clippers",
+        "Los Angeles Lakers",
+        "Memphis Grizzlies",
+        "Miami Heat",
+        "Milwaukee Bucks",
+        "Minnesota Timberwolves",
+        "New Orleans Pelicans",
+        "New York Knicks",
+        "Oklahoma City Thunder",
+        "Orlando Magic",
+        "Philadelphia 76ers",
+        "Phoenix Suns",
+        "Portland Trail Blazers",
+        "Sacramento Kings",
+        "San Antonio Spurs",
+        "Toronto Raptors",
+        "Utah Jazz",
+        "Washington Wizards"
+      ];
+      res.render("index");
+      
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
